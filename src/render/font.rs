bdc6c41e28dbebaccb8752b65093ecb5071672b5
trolleyman <cgtrolley@gmail.com>
2016-04-02 20:25:23 +0100

    Fixed issue where first character of strings was not rendering

--- src/render/font.rs
+++ src/render/font.rs
@@ -5,10 +5,10 @@ use std::borrow::Cow;
 use glium::{Blend, BlendingFunction, LinearBlendingFactor, Texture2d, Program, Surface, VertexBuffer, IndexBuffer, DrawParameters, BackfaceCullingMode};
 use glium::Rect as GlRect;
 use glium::index::PrimitiveType;
-use glium::backend::{Context};
+use glium::backend::Context;
 use glium::texture::{RawImage2d, ClientFormat, UncompressedFloatFormat, MipmapsOption};
 use rusttype::*;
-use rusttype::gpu_cache::Cache;
+use rusttype::gpu_cache::{Cache, CacheWriteErr};
 use cgmath::{SquareMatrix, Matrix4, vec3};
 
 use render::{load_shader, load_font, Color};
@@ -70,7 +70,7 @@ impl State {
 	}
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct FontVertex {
 	pub pos: [f32; 2],
 	pub uv : [f32; 2],
@@ -138,6 +138,7 @@ impl FontRender {
 			},
 		};
 		
+		//println!("Rendering string: {}", s);
 		let mut glyphs = Vec::new();
 		
 		let mut state = State::new(x, y, scale, &font);
@@ -145,7 +146,6 @@ impl FontRender {
 		let mut cprev = None;
 		for c in s.chars() {
 			if let Some(glyph) = state.layout_char(&font, cprev, c) {
-				//println!("Laying out char {}: {:?}", c, glyph.pixel_bounding_box());
 				glyphs.push(glyph);
 			}
 			cprev = Some(c);
@@ -156,17 +156,8 @@ impl FontRender {
 	}
 }
 
-fn get_glyph(font_tex: &mut Texture2d, cache: &mut Cache, glyph: &PositionedGlyph) -> Option<(Rect<f32>, Rect<i32>)> {
-	match cache.rect_for(0, glyph) {
-		Ok(ret) => return ret,
-		Err(_) => {
-			// TODO: extern crate log;
-			//trace!("Cache read miss for glyph: {:?}", glyph);
-		},
-	}
-	
-	cache.queue_glyph(0, glyph.clone());
-	match cache.cache_queued(|rect: Rect<u32>, data| {
+fn cache_queued(font_tex: &mut Texture2d, cache: &mut Cache) -> Result<(), CacheWriteErr> {
+	cache.cache_queued(|rect: Rect<u32>, data| {
 		let rect = GlRect {
 			left:   rect.min.x,
 			bottom: rect.min.y,
@@ -181,33 +172,14 @@ fn get_glyph(font_tex: &mut Texture2d, cache: &mut Cache, glyph: &PositionedGlyp
 		};
 		
 		font_tex.write(rect, data);
-	}) {
-		Ok(_) => {},
-		Err(_) => {
-			//writeln!(io::stderr(), "Error: The glyph {:?} was not uploaded to the GPU cache.", glyph);
-			return None;
-		},
-	}
-	// FIXME: if the string is so long that the cache is filled up and overwrites the first character.
-	match cache.rect_for(0, glyph) {
-		Ok(ret) => ret,
-		Err(_) => {
-			//writeln!(io::stderr(), "Error: The glyph {:?} was not uploaded to the GPU cache.", glyph);
-			None
-		},
-	}
+	})
 }
 
-fn draw_glyph(font_tex: &mut Texture2d, cache: &mut Cache, glyph: &PositionedGlyph, vs: &mut Vec<FontVertex>, is: &mut Vec<u32>) {
-	if let Some((uv, pos)) = get_glyph(font_tex, cache, glyph) {
+fn draw_glyph(cache: &mut Cache, glyph: &PositionedGlyph, vs: &mut Vec<FontVertex>, is: &mut Vec<u32>) {
+	if let Ok(Some((uv, pos))) = cache.rect_for(0, glyph) {
 		// 0--1
 		// |  |
 		// 2--3
-		vs.push(FontVertex::new([pos.min.x as f32, pos.min.y as f32], [uv.min.x, uv.min.y]));
-		vs.push(FontVertex::new([pos.max.x as f32, pos.min.y as f32], [uv.max.x, uv.min.y]));
-		vs.push(FontVertex::new([pos.min.x as f32, pos.max.y as f32], [uv.min.x, uv.max.y]));
-		vs.push(FontVertex::new([pos.max.x as f32, pos.max.y as f32], [uv.max.x, uv.max.y]));
-		
 		let i = vs.len() as u32;
 		is.push(i);
 		is.push(i+2);
@@ -216,49 +188,76 @@ fn draw_glyph(font_tex: &mut Texture2d, cache: &mut Cache, glyph: &PositionedGly
 		is.push(i+1);
 		is.push(i+2);
 		is.push(i+3);
+		
+		vs.push(FontVertex::new([pos.min.x as f32, pos.min.y as f32], [uv.min.x, uv.min.y]));
+		vs.push(FontVertex::new([pos.max.x as f32, pos.min.y as f32], [uv.max.x, uv.min.y]));
+		vs.push(FontVertex::new([pos.min.x as f32, pos.max.y as f32], [uv.min.x, uv.max.y]));
+		vs.push(FontVertex::new([pos.max.x as f32, pos.max.y as f32], [uv.max.x, uv.max.y]));
 	}
 }
 
 fn draw_glyphs<S: Surface>(ctx: &Rc<Context>, surface: &mut S, shader: &Program, font_tex: &mut Texture2d, cache: &mut Cache, size: (f32, f32), glyphs: &[PositionedGlyph], color: Color) {
-	let mut vs = Vec::new();
-	let mut is = Vec::new();
-	
-	// Put glyphs into buffer
-	for glyph in glyphs.iter() {
-		draw_glyph(font_tex, cache, glyph, &mut vs, &mut is);
-	}
-	
 	// Calculate matrix
 	let (w, h) = size;
 	let mut mat = Matrix4::<f32>::identity();
 	mat = mat * Matrix4::from_nonuniform_scale(1.0, -1.0, 1.0);
 	mat = mat * Matrix4::from_translation(vec3(-1.0, -1.0, 0.0));
 	mat = mat * Matrix4::from_nonuniform_scale(2.0 / w, 2.0 / h, 1.0);
-	
-	// Upload buffer
-	let vs = VertexBuffer::immutable(ctx, &vs).unwrap();
-	let is = IndexBuffer ::immutable(ctx, PrimitiveType::TrianglesList, &is).unwrap();
-	// Draw buffer
-	surface.draw(
-		&vs,
-		&is,
-		&shader,
-		&uniform!{
-			tex:   &*font_tex,
-			color: <Color as Into<[f32; 3]>>::into(color),
-			mat:   <Matrix4<f32> as Into<[[f32; 4]; 4]>>::into(mat),
-		},
-		&DrawParameters {
-			blend: Blend {
-				color: BlendingFunction::Addition{
-					source:      LinearBlendingFactor::SourceAlpha,
-					destination: LinearBlendingFactor::OneMinusSourceAlpha,
+	draw_glyphs_mat(ctx, surface, shader, font_tex, cache, mat, glyphs, color)
+}
+
+fn draw_glyphs_mat<S: Surface>(ctx: &Rc<Context>, surface: &mut S, shader: &Program, font_tex: &mut Texture2d, cache: &mut Cache, mat: Matrix4<f32>, glyphs: &[PositionedGlyph], color: Color) {
+	cache.clear_queue();
+	for glyph in glyphs.iter() {
+		cache.queue_glyph(0, glyph.clone());
+	}
+	match cache_queued(font_tex, cache) {
+		Ok(()) => {
+			//println!("Rendering glyphs: {:?}", glyphs.iter().map(|g| g.id()).collect::<Vec<_>>());
+			let mut vs = Vec::new();
+			let mut is = Vec::new();
+			
+			for glyph in glyphs {
+				draw_glyph(cache, glyph, &mut vs, &mut is);
+			}
+			
+			// Upload buffer
+			let vs = VertexBuffer::immutable(ctx, &vs).unwrap();
+			let is = IndexBuffer ::immutable(ctx, PrimitiveType::TrianglesList, &is).unwrap();
+			// Draw buffer
+			surface.draw(
+				&vs,
+				&is,
+				&shader,
+				&uniform!{
+					tex:   &*font_tex,
+					color: <Color as Into<[f32; 3]>>::into(color),
+					mat:   <Matrix4<f32> as Into<[[f32; 4]; 4]>>::into(mat),
 				},
-				alpha: BlendingFunction::Max,
-				..Default::default()
-			},
-			backface_culling: BackfaceCullingMode::CullClockwise,
-			..Default::default()
+				&DrawParameters {
+					blend: Blend {
+						color: BlendingFunction::Addition{
+							source:      LinearBlendingFactor::SourceAlpha,
+							destination: LinearBlendingFactor::OneMinusSourceAlpha,
+						},
+						alpha: BlendingFunction::Max,
+						..Default::default()
+					},
+					backface_culling: BackfaceCullingMode::CullClockwise,
+					..Default::default()
+				}
+			).ok();
+		},
+		Err(e) => {
+			if glyphs.len() <= 1 {
+				// Error: cannot put glyph in cache. // TODO: Clean up error message // TODO: Maybe render default glyph?
+				writeln!(io::stderr(), "Error: Cannot render glyph: {:?}", e).ok();
+			} else {
+				// Split glyphs up into two halves, and draw them seperately.
+				let (a, b) = glyphs.split_at(glyphs.len() / 2);
+				draw_glyphs_mat(ctx, surface, shader, font_tex, cache, mat, a, color);
+				draw_glyphs_mat(ctx, surface, shader, font_tex, cache, mat, b, color);
+			}
 		}
-	).ok();
+	}
 }
