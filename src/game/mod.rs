843756017bd4fce6a5aa63eed5b3d500f10b0e7e
trolleyman <cgtrolley@gmail.com>
2016-04-09 02:00:37 +0100

    Changed re-exports in game module

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -5,7 +5,7 @@ mod entity;
 mod key;
 
 pub use self::state::State as GameState;
-pub use self::state::Gravity;
+pub use self::state::{EntityId, Gravity};
 pub use self::game::Game;
-pub use self::entity::{Entity, EntityBuilder};
+pub use self::entity::{Entity, EntityBuilder, Component, ComponentHandle};
 pub use self::key::KeyboardState;
