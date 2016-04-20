c894082abef1b05c944aa98fd8ab95c781abbf42
unknown <cgtrolley@gmail.com>
2016-04-20 12:46:45 +0100

    Simplified test case

--- src/game/entity.rs
+++ src/game/entity.rs
@@ -1,36 +1,5 @@
-use std::rc::Rc;
-
-use na::{Pnt3, Vec3, Sim3, Eye, Rot3, Rotation, Iso3, Translation, ToHomogeneous};
-use nc::shape::{Ball3, Ball};
-use np::world::World;
-use np::object::{RigidBody, RigidBodyHandle};
-use np::volumetric::Volumetric;
-
-use game::GameState;
-use render::{Render, RenderableMesh};
 
 pub struct Entity {
-	/// Visible mesh
-	mesh: Box<RenderableMesh>,
-	/// Density
-	mass: f32,
 }
 impl Entity {
-	/// Constructs an entity and adds it to `world`, returning it's ID
-	pub fn new(mesh: Box<RenderableMesh>, mass: f32) -> Entity {
-		Entity {
-			mesh: mesh,
-			mass: mass,
-		}
-	}
-	
-	/// Returns the mass of the object
-	pub fn mass(&self) -> f32 {
-		self.mass
-	}
-	
-	/// Renders the entity
-	pub fn render(&self, r: &mut Render, position: Iso3<f32>) {
-		self.mesh.render(r, position.to_homogeneous());
-	}
 }
--- src/game/game.rs
+++ src/game/game.rs
@@ -8,16 +8,6 @@ use settings::Settings;
 use util::DurationExt;
 
 pub struct Game {
-	render: Render,
-	settings: Settings,
-	
-	current_state: GameState,
-	running: bool,
-	keyboard_state: KeyboardState,
-	mouse_state: (i32, i32),
-	focused: bool,
-	step: bool,
-	ignore_next_mouse_movement: bool,
 }
 impl Game {
 	pub fn new(settings: Settings, cam: Camera) -> Game {
@@ -31,180 +21,17 @@ impl Game {
 
 	pub fn with_state(settings: Settings, render: Render, state: GameState) -> Game {
 		Game {
-			render: render,
-			settings: settings,
-			
-			current_state: state,
-			running: true,
-			keyboard_state: KeyboardState::new(),
-			mouse_state: (0, 0),
-			focused: true,
-			step: false,
-			ignore_next_mouse_movement: false,
 		}
 	}
 	
 	pub fn main_loop(&mut self) {
-		// Try and focus on the game window
-		self.focused = self.render.try_focus().is_ok();
 		
-		// How long each physics timestep should be.
-		let sec = Duration::new(1, 0);
-		let physics_dt = sec / 120;
-		
-		let mut lag = Duration::from_millis(0);
-		let mut previous = Instant::now();
-		
-		let mut previous_fps_count = Instant::now();
-		let mut frames = 0;
-		let mut fps = 0;
-		
-		info!("Starting game main loop");
-		while self.running {
-			// Process timing stuff
-			let current = Instant::now();
-			let elapsed = current - previous;
-			previous = current;
-			lag += elapsed;
-			
-			// Calculate fps
-			if current - previous_fps_count >= sec {
-				previous_fps_count = Instant::now();
-				fps = frames;
-				frames = 0;
-			}
-			
-			// Process events
-			self.process_events();
-			
-			// Tick game
-			let mut n = 0;
-			while lag >= physics_dt {
-				n += 1;
-				lag -= physics_dt;
-			}
-			self.tick(physics_dt.as_secs_partial() as f32, n);
-			
-			// Render to screen
-			// TODO: Render using seperate thread (mutexes?).
-			self.current_state.render(&mut self.render, fps);
-			frames += 1;
-		}
-	}
-	
-	pub fn process_events(&mut self) {
-		let (mp_x, mp_y) = self.render.get_window().and_then(|w| w.get_outer_size()).unwrap_or((0, 0));
-		let (mp_x, mp_y) = (mp_x as i32 / 2, mp_y as i32 / 2);
-		if self.focused {
-			self.render.get_window().map(|w| w.set_cursor_position(mp_x, mp_y));
-		}
-		
-		if self.step {
-			self.step = false;
-			self.settings.paused = true;
-		}
-		
-		let mut resized = false;
-		let mut mouse_pos = (mp_x, mp_y);
-		for e in self.render.poll_events() {
-			if let &Event::MouseMoved(_) = &e {
-				trace!("Event recieved: {:?}", e);
-			} else {
-				debug!("Event recieved: {:?}", e);
-			}
-			
-			match e {
-				Event::Closed => {
-					self.running = false;
-					return; // Ignore all other events.
-				},
-				Event::MouseMoved(pos) => {
-					if self.ignore_next_mouse_movement {
-						self.ignore_next_mouse_movement = false;
-					} else {
-						mouse_pos = pos;
-					}
-				},
-				Event::Focused(b) => {
-					if b {
-						info!("Window focused");
-					} else {
-						info!("Window unfocused");
-						self.focused = false;
-					}
-				},
-				Event::MouseInput(mouse_state, button) => {
-					if mouse_state == ElementState::Pressed && button == MouseButton::Left {
-						if !self.focused {
-							self.render.get_window().map(|w| w.set_cursor_position(mp_x, mp_y));
-							mouse_pos = (mp_x, mp_y);
-							self.ignore_next_mouse_movement = true;
-							self.focused = true;
-						}
-					}
-				},
-				Event::Resized(_, _) => {
-					resized = true;
-				},
-				Event::KeyboardInput(key_state, _, Some(code)) => {
-					self.keyboard_state.process_event(key_state, code);
-					if code == VirtualKeyCode::Escape {
-						self.focused = false;
-					}
-					if key_state == ElementState::Pressed && Some(code) == self.settings.physics_pause {
-						self.settings.paused = !self.settings.paused;
-						if self.settings.paused {
-							info!("Game paused");
-						} else {
-							info!("Game resumed");
-						}
-					}
-					if key_state == ElementState::Pressed && Some(code) == self.settings.physics_step {
-						if self.settings.paused {
-							self.settings.paused = false;
-							self.step = true;
-							info!("Game stepped");
-						}
-					}
-				},
-				_ => {},
-			}
-		}
-		
-		if resized {
-			debug!("Resizing renderer");
-			// Resize
-			self.render.resize();
-		}
-		
-		if self.focused {
-			self.render.input_grab();
-		} else {
-			self.render.input_normal();
-		}
-		
-		if self.focused /*&& !ignore_movement_frame*/ {
-			let xdiff = mouse_pos.0 - mp_x;
-			let ydiff = mouse_pos.1 - mp_y;
-			//println!("mouse_pos: {:?}, mp_x: {}, mp_y: {}, xdiff: {}, ydiff: {}", mouse_pos, mp_x, mp_y, xdiff, ydiff);
-			self.mouse_state = (xdiff, ydiff);
-		} else {
-			self.mouse_state = (0, 0);
-		}
 	}
 	
 	/// Ticks the game.
 	/// `dt` is the number of seconds since last frame.
 	/// `n` is the number of iterations to do.
 	pub fn tick(&mut self, dt: f32, n: u32) {
-		if n == 0 {
-			return;
-		}
-		trace!("Game tick: {}s ({} iterations)", dt, n);
-		// Tick next state
-		for _ in 0..n {
-			self.current_state.tick(dt, &self.settings, &self.keyboard_state, self.mouse_state);
-			self.mouse_state = (0, 0);
-		}
+		
 	}
 }
