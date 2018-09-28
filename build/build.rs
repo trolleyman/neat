9291729aae5a2148d9a6105d23dc28cfcc8cbb92
Callum Tolley <cgtrolley@gmail.com>
2018-09-28 23:35:23 +0100

    Changed build script to symlink assets dir when in debug mode

--- build/build.rs
+++ build/build.rs
@@ -2,21 +2,60 @@ use std::path::{Path, PathBuf};
 use std::fs;
 use std::env;
 
+#[cfg(windows)]
+fn symlink_dir(src: &Path, dst: &Path) {
+	::std::os::windows::fs::symlink_dir(src, dst).unwrap();
+}
+
+#[cfg(unix)]
+fn symlink_dir(src: &Path, dst: &Path) {
+	::std::os::unix::fs::symlink(src, dst).unwrap();
+}
+
+#[cfg(all(not(windows), not(unix)))]
+fn symlink_dir(src: &Path, dst: &Path) {
+	panic!("unsupported platform");
+}
+
 /// Recursively copy a folder
 fn copy_folder(src: &Path, dst: &Path) {
 	assert!(src.is_dir());
-	fs::create_dir_all(dst).unwrap();
-	assert!(dst.is_dir());
-	for entry in fs::read_dir(src).unwrap() {
-		let entry = entry.unwrap();
-		let ty = entry.file_type().unwrap();
-		let dst = dst.join(entry.path().file_name().unwrap());
-		if ty.is_dir() {
-			copy_folder(&entry.path(), &dst);
-		} else if ty.is_file() {
-			fs::copy(&entry.path(), &dst).unwrap();
-			println!("Copied '{}' => '{}'", entry.path().display(), dst.display());
-		} // Ignore symbolic links
+	if env::var("PROFILE").unwrap() == "release" {
+		println!("cargo:rerun-if-changed={}", src.display());
+		println!("Release mode -- copying all files to output dir");
+		
+		// Create dst dir
+		fs::create_dir_all(dst).unwrap();
+		assert!(dst.is_dir());
+		println!("Created dir {}", dst.display());
+		
+		// Copy all files recursively
+		for entry in fs::read_dir(src).unwrap() {
+			let entry = entry.unwrap();
+			println!("cargo:rerun-if-changed={}", entry.path().display());
+			let ty = entry.file_type().unwrap();
+			let dst = dst.join(entry.path().file_name().unwrap());
+			if ty.is_dir() {
+				copy_folder(&entry.path(), &dst);
+			} else if ty.is_file() {
+				fs::copy(&entry.path(), &dst).unwrap();
+				println!("Copied '{}' => '{}'", entry.path().display(), dst.display());
+			}
+			// Ignore symbolic links
+		}
+	} else {
+		println!("Debug mode -- symlinking");
+		// Remove previous data
+		if let Ok(meta) = fs::symlink_metadata(dst) {
+			let ty = meta.file_type();
+			if ty.is_dir() {
+				fs::remove_dir_all(dst).unwrap();
+			} else {
+				fs::remove_file(dst).unwrap();
+			}
+		}
+		symlink_dir(src, dst);
+		println!("Symlinked '{}' => '{}'", dst.display(), src.display());
 	}
 }
 
